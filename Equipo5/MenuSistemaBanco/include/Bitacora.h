9f11dc35f933c5b2be44cacd008b06452054efbf
Ivan774gt <deleonboris6@gmail.com>
2025-05-05 09:44:33 -0600

    Bitacora ver1 por Boris de Leon 9959-24-6203

--- Equipo5/MenuSistemaBanco/include/Bitacora.h
+++ Equipo5/MenuSistemaBanco/include/Bitacora.h
@@ -1,20 +1,13 @@
+//Primera version por Boris de Leon 9959-24-6203
 #ifndef BITACORA_H
 #define BITACORA_H
-#include<iostream>
 
-using std::string;
+#include <string>
 
-class bitacora
-{
-    public:
-        void menu();
-        void insertar(string nombre, string aplicacion, string accion);
-        void desplegar();
-    protected:
-
-    private:
-		string nombre, aplicacion, accion;
-
+class Bitacora {
+public:
+    void insertar(const std::string& usuario, int codigo, const std::string& aplicacion, const std::string& accion);
+    void mostrar();
 };
-#endif // BITACORA_H
 
+#endif
