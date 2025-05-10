82e55f6a4d93accc4275f68fd3a3dfad1d23d9bb
meilyn garcia <grcmeilyn@gmail.com>
2025-05-09 19:29:05 -0600

    bitacora

--- Equipo7/main.cpp
+++ Equipo7/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "SistemaVentas.h"
 #include "Login.h"
+#include "bitacora.h"
 
 using namespace std;
+
+
+string usuarioActual; // Para guardar el nombre del usuario bitacora
 
 void mostrarMenuPrincipal();
 
@@ -19,10 +23,11 @@ int main() {
         cout << "3. Salir\n";
         cout << "Seleccione una opcion: ";
         cin >> opcion;
+        cin.ignore(); // Para evitar errores con getline
 
         switch (opcion) {
             case 1:
-                accesoPermitido = iniciarSesion();
+                accesoPermitido = iniciarSesion(usuarioActual);
                 break;
             case 2:
                 registrarUsuario();
@@ -36,6 +41,7 @@ int main() {
     } while (!accesoPermitido);
 
     mostrarMenuPrincipal();
+     registrarEvento(usuarioActual, "Cerró sesión desde menú principal");
     return 0;
 }
 
@@ -58,18 +64,23 @@ void mostrarMenuPrincipal() {
 
         switch (opcion) {
             case 1:
+                registrarEvento(usuarioActual, "Entró a la opción Archivo");
                 cout << "Archivo (sin funciones aun).\n";
                 break;
             case 2:
-                sistema.mostrarMenu(); // reutilizamos temporalmente
+                registrarEvento(usuarioActual, "Entró a la opción Catálogos");
+                sistema.mostrarMenu();
                 break;
             case 3:
+                registrarEvento(usuarioActual, "Entró a la opción Procesos");
                 sistema.realizarVenta();
                 break;
             case 4:
+                registrarEvento(usuarioActual, "Entró a la opción Informes");
                 sistema.mostrarVentas();
                 break;
             case 5:
+                registrarEvento(usuarioActual, "Salió del menú general");
                 cout << "Cerrando sesion...\n";
                 break;
             default:
